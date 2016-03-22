6f1ff65004ff74c26615863ae06c5dd1de08a030
Mathias Tjernberg <mathias.tjernberg@gmail.com>
2016-03-22 18:31:32 +0100

    Project created

---
+++ RSS_Mathias/RSS_Mathias/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSS_Mathias
+//
+//  Created by Mathias Tjernberg on 2016-03-22.
+//  Copyright © 2016 Mathias Tjernberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
